a64c63883effd70627a7a3809d2eec11d3cded86
RiCiT <tamas.balint.farago@gmail.com>
2025-10-29 00:56:52 +0100

    can now make an executable from the generated asm and if ran it gives out the appropriate exit code!:))

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,51 @@
 #include <optional>
 #include <vector>
 
-enum class TokenType {
+enum class TokenType 
+{
     exit,
     int_lit,
     semi
 };
 
-struct Token {
+struct Token 
+{
     TokenType type;
     std::optional<std::string> value;
 };
 
-std::vector<Token> tokenize(const std::string& str) {
+std::vector<Token> tokenize(const std::string& str) 
+{
     std::vector<Token> tokens;
 
     std::string buf;
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length(); i++) 
+    {
         char c = str.at(i);
         if (std::isalpha(c)) {
             buf.push_back(c);
             i++;
-            while (std::isalnum(str.at(i))) {
+            while (std::isalnum(str.at(i))) 
+            {
                 buf.push_back(str.at(i));
                 i++;
             }
             i--;
 
-            if (buf == "exit") {
+            if (buf == "exit") 
+            {
                 tokens.push_back({.type = TokenType::exit});
                 buf.clear();
                 continue;
             }
-            else {
+            else 
+            {
                 std::cerr << "You messed up!" << std::endl;
                 exit(EXIT_FAILURE);
             }
         }
-        else if (std::isdigit(c)) {
+        else if (std::isdigit(c)) 
+        {
             buf.push_back(c);
             i++;
             while (std::isdigit(str.at(i)))
@@ -52,13 +60,16 @@ std::vector<Token> tokenize(const std::string& str) {
             tokens.push_back({.type = TokenType::int_lit, .value = buf});
             buf.clear();
         }
-        else if (c == ';') {
+        else if (c == ';') 
+        {
             tokens.push_back({.type = TokenType::semi});
         }
-        else if (std::isspace(c)) {
+        else if (std::isspace(c)) 
+        {
             continue;
         }
-        else {
+        else 
+        {
             std::cerr << "You screwed up" << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -67,14 +78,38 @@ std::vector<Token> tokenize(const std::string& str) {
     return tokens;
 }
 
-int main(int argc, char* argv[]) {
+std::string tokens_to_asm(std::vector<Token>& tokens) 
+{
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+    for (int i = 0; i < tokens.size(); i++) 
+    {
+        const Token& token = tokens.at(i);
+        if (token.type == TokenType::exit) 
+        {
+            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) 
+            {
+                if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) 
+                {
+                    output << "    mov rax, 60\n";
+                    output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+                    output << "    syscall";
+                }
+                
+            }   
+        }
+    }
+    return output.str();
+}
+
+int main(int argc, char* argv[]) 
+{
     if(argc != 2) {
         std::cerr << "Incorrect usage. Corrrect usage: " << std::endl;
         std::cerr << "cobble <input.cb>" << std::endl;
         return EXIT_FAILURE;
     }
-
-    
+     
     std::string contents;
     {
         std::stringstream content_stream;
@@ -83,7 +118,15 @@ int main(int argc, char* argv[]) {
         contents = content_stream.str();
     }
 
-    std::vector<Token> thing = tokenize(contents);
+    std::vector<Token> tokens = tokenize(contents);
+
+    {
+        std::fstream file("out.asm", std::ios::out);
+        file << tokens_to_asm(tokens);
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
 
     return EXIT_SUCCESS;
 } 
