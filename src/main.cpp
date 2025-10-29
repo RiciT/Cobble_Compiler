d00d89906e42b8829494ed41996217e841a9ffa1
RiCiT <tamas.balint.farago@gmail.com>
2025-10-29 16:41:04 +0100

    tokenizer refactored and separated

--- src/main.cpp
+++ src/main.cpp
@@ -4,78 +4,11 @@
 #include <optional>
 #include <vector>
 
-enum class TokenType 
-{
-    exit,
-    int_lit,
-    semi
-};
-
-struct Token 
-{
-    TokenType type;
-    std::optional<std::string> value;
-};
+#include "./tokenization.hpp"
 
 std::vector<Token> tokenize(const std::string& str) 
 {
-    std::vector<Token> tokens;
-
-    std::string buf;
-    for (int i = 0; i < str.length(); i++) 
-    {
-        char c = str.at(i);
-        if (std::isalpha(c)) {
-            buf.push_back(c);
-            i++;
-            while (std::isalnum(str.at(i))) 
-            {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-
-            if (buf == "exit") 
-            {
-                tokens.push_back({.type = TokenType::exit});
-                buf.clear();
-                continue;
-            }
-            else 
-            {
-                std::cerr << "You messed up!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (std::isdigit(c)) 
-        {
-            buf.push_back(c);
-            i++;
-            while (std::isdigit(str.at(i)))
-            {
-               buf.push_back(str.at(i));
-               i++; 
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_lit, .value = buf});
-            buf.clear();
-        }
-        else if (c == ';') 
-        {
-            tokens.push_back({.type = TokenType::semi});
-        }
-        else if (std::isspace(c)) 
-        {
-            continue;
-        }
-        else 
-        {
-            std::cerr << "You screwed up" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
     
-    return tokens;
 }
 
 std::string tokens_to_asm(std::vector<Token>& tokens) 
