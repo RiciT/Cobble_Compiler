06af1e9d3b7aaf3d80db45ebeb0595c9ff2ed5ed
RiCiT <tamas.balint.farago@gmail.com>
2025-11-22 21:27:10 +0100

    update

---
+++ cmake-build-debug/CMakeFiles/cobble.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cobble.dir/link.d"
+  "CMakeFiles/cobble.dir/src/main.cpp.o"
+  "CMakeFiles/cobble.dir/src/main.cpp.o.d"
+  "cobble"
+  "cobble.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cobble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
